3cb59493410c315f588a6d4161e8a3d0c19b726e
mhammedyildirim <61433710+mhammedyildirim@users.noreply.github.com>
2020-03-10 00:13:14 +0300

    Initial commit

---
+++ tek_cift_hesaplama/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tek_cift_hesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sayi giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi % 2 == 0)
+            {
+                Console.WriteLine("Sayi cifttir.");
+            }
+            else
+            {
+                Console.WriteLine("Sayi tektir.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
